635da524c05e8f8b0137a0813b4e7c85310d1099
Vinícius josé <113718186+viniciusjos@users.noreply.github.com>
2024-05-16 11:46:49 -0300

    Add files via upload
    16/mai/24

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-const botoes = document.querySelectorA11(."botao");
+const botoes = document.querySelectorAll(".botao");
 
-for(let i=0;i < botoes.length;i++){
-    botoes [i] .onclick = function(){
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
 
-for(let j=0;j<botoes.length;j++){
-    botoes[j].classList.remove("ativo")
-}
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
 
-        botoes [i].classList.add("ativo");
+        botoes[i].classList.add("ativo");
     }
-    console.log();
 }
